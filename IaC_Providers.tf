60674bedae2614272b2b7acd93bf8f39de5094d9
shrikantgcet <shrikant.gcet@gmail.com>
2024-04-18 23:06:31 +1200

    Add files via upload

---
+++ IaC_Providers.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.99.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+data "azurerm_client_config" "current" {}
+
+
