8831f3d3d87b50f6fe1fe5e34a4436d491992050
v.jaiswar <v.jaiswar@nzba.org>
2024-04-25 00:00:40 +1200

    params updated

--- terraform/ApplicationInsights.tf
+++ terraform/ApplicationInsights.tf
@@ -3,7 +3,7 @@ resource "azurerm_application_insights" "ai" {
   name                = var.ai_name
   location                  = azurerm_resource_group.iac_rg.location
   resource_group_name       = azurerm_resource_group.iac_rg.name
-  application_type    = var.ai_application_type
+  application_type    =       "other"
 
     tags = merge(
     var.tags,
