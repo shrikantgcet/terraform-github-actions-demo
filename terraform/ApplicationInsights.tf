dff26c5018016caad0075230ec3b5eb988947093
v.jaiswar <v.jaiswar@nzba.org>
2024-04-29 21:52:06 +1200

    ai name updated

--- terraform/ApplicationInsights.tf
+++ terraform/ApplicationInsights.tf
@@ -3,6 +3,7 @@ resource "azurerm_application_insights" "ai" {
   name                = var.ai_name
   location                  = azurerm_resource_group.iac_rg.location
   resource_group_name       = azurerm_resource_group.iac_rg.name
+  workspace_id        = azurerm_log_analytics_workspace.la_ws.id
   application_type    =       "other"
 
     tags = merge(
