3860446f39445bf6b8bc7f6b62841cc27824e8c7
v.jaiswar <v.jaiswar@nzba.org>
2024-04-29 23:01:37 +1200

    ai name updated

--- terraform/StorageAccount.tf
+++ terraform/StorageAccount.tf
@@ -23,11 +23,6 @@ resource "azurerm_storage_account" "sto" {
   account_tier             = var.sto_account_tier
   account_replication_type = var.sto_account_replication_type
 
-  blob_properties {
-    delete_retention_policy {
-      days = 7
-    }
-  }
   
   tags = merge(
     var.tags,
